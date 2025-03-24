4e13ad4952b2a220a1322ffc0788663c69cea6fb
Gaubee <gaubeebangeel@gmail.com>
2025-03-24 23:29:18 +0800

    🚑

--- appn-lit/docs-src/_includes/footer.11ty.ts
+++ appn-lit/docs-src/_includes/footer.11ty.ts
@@ -4,7 +4,9 @@ export default function (data: EleventyData): string {
   const html = String.raw;
   return html`<footer class="bg-ani">
     <ol>
-      <li><a href="https://github.com/gaubee/appn">Github</a></li>
+      <li>
+        <a target="_blank" href="https://github.com/gaubee/appn">Github</a>
+      </li>
     </ol>
   </footer>`;
 }
