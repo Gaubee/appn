e313d9a3e9f8e88705414f8a309dc0bc979ba62b
Gaubee <GaubeeBangeel@Gmail.com>
2025-05-03 17:26:19 +0000

    Deploying to gh-pages from @ Gaubee/appn@f8d5fe94669a086be7224b57be7d6264c0c1641e 🚀

--- assets/index-8p1QgIjV.js
+++ assets/index-8p1QgIjV.js
@@ -1,4 +1,4 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";import"./starting-style-demo-BRY0SUH6.js";import"./index-nJykmiXK.js";import"./css-color-mix-Bc6Os1iC.js";class s extends HTMLElement{constructor(){super(),this.attachShadow({mode:"open"});const t=String.raw;this.shadowRoot.innerHTML=t`
+import"./modulepreload-polyfill-B5Qt9EMX.js";import"./starting-style-demo-BRY0SUH6.js";import"./index-BfTluGWP.js";import"./css-color-mix-Bc6Os1iC.js";class s extends HTMLElement{constructor(){super(),this.attachShadow({mode:"open"});const t=String.raw;this.shadowRoot.innerHTML=t`
       <slot></slot>
       <css-starting-style mode="native" host selector=".native.showing" cssText="background-color: red!important;"></css-starting-style>
       <css-starting-style mode="shim" host selector=".shim.showing" cssText="background-color: red!important;"></css-starting-style>
