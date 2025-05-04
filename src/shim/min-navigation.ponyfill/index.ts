2be368c495e999eb86cab912eb38675a1a1d9bf3
Gaubee <gaubeebangeel@gmail.com>
2025-05-04 21:08:08 +0800

    🚑 修复min-nav启动问题

--- src/shim/min-navigation.ponyfill/index.ts
+++ src/shim/min-navigation.ponyfill/index.ts
@@ -1,17 +1,14 @@
-console.log(0);
-import {MinNavigation} from './navigation';
-import {getState} from './storage';
-
-export * from '../navigation.native/types';
 export * from './navigate-event';
 export * from './navigation';
 export * from './navigation-current-entry-change-event';
 export * from './navigation-destination';
 export * from './navigation-history-entry';
 export * from './navigation-transition';
-console.log(111);
+
+import {MinNavigation} from './navigation';
+import {getState} from './storage';
+
 export const minNavigation = new MinNavigation(await getState());
-console.log(222);
 
 Object.assign(globalThis, {
   minNavigation,
