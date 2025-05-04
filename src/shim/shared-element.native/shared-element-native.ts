3daadb94fa64c19269928d71f2fafdfd73510035
Gaubee <gaubeebangeel@gmail.com>
2025-05-04 21:44:54 +0800

    ⚡ 现在在native模式下，不会触发没必要的view-transiton-name，避免浪费性能
    和ponyfill保持类似的逻辑

--- src/shim/shared-element.native/shared-element-native.ts
+++ src/shim/shared-element.native/shared-element-native.ts
@@ -1,5 +1,6 @@
 import {CssSheetArray} from '@gaubee/web';
 import {match} from 'ts-pattern';
+import {set_intersection} from '../set.polyfill';
 import {SharedElementBaseImpl, sharedElementLifecycle, sharedElements} from './shared-element-common';
 import {
   type SharedElementAnimation,
@@ -45,38 +46,50 @@ export class SharedElement extends SharedElementBaseImpl implements SharedElemen
       return;
     }
     const sharedElementPagesContext = this.__getPagesContext(lifecycle, context);
+    const {dest, from} = sharedElementPagesContext;
+    // 必须确保两个page都存在
+    if (dest == null || from == null) return;
 
     /// 最后，处理过渡元素
     if (lifecycle === 'finish') {
-      for (const pageItem of [sharedElementPagesContext.from, sharedElementPagesContext.dest]) {
-        if (pageItem) {
-          sharedElementLifecycle.delete(pageItem.navEntryNode);
-          /// 清理所有 viewTransitionName
-          // pageItem.node.style.viewTransitionName = '';
-          for (const sharedElement of sharedElements.queryAll(pageItem.navEntryNode)) {
-            sharedElement.style.viewTransitionName = '';
-            delete sharedElement.dataset.sharedElementState;
-          }
+      for (const pageItem of [from, dest]) {
+        sharedElementLifecycle.delete(pageItem.navEntryNode);
+        /// 清理所有 viewTransitionName
+        // pageItem.node.style.viewTransitionName = '';
+        for (const sharedElement of sharedElements.queryAll(pageItem.navEntryNode)) {
+          sharedElement.style.viewTransitionName = '';
+          delete sharedElement.dataset.sharedElementState;
         }
       }
     } else {
+      const fromNavEntryNode = from.navEntryNode;
+      const destNavEntryNode = dest.navEntryNode;
+
       const sharedElementCss = this.css;
       const sharedElementMap = new Map<string, HTMLElement>();
-      const sharedElementIndex = Math.max(sharedElementPagesContext.from?.navEntryNode.sharedIndex ?? 0, sharedElementPagesContext.dest?.navEntryNode.sharedIndex ?? 0) + 1;
-      const sharedElementPages = match(lifecycle)
-        .with('first', () => [sharedElementPagesContext.dest, sharedElementPagesContext.from])
-        .with('last', () => [sharedElementPagesContext.from, sharedElementPagesContext.dest])
+      const sharedElementIndex = Math.max(from.navEntryNode.sharedIndex, dest.navEntryNode.sharedIndex) + 1;
+      // 必须确保同一个name在两个page都都存在
+      const fromSharedElementMap = new Map(sharedElements.queryAllWithConfig(fromNavEntryNode).map((item) => [item.name, item]));
+      const destSharedElementMap = new Map(sharedElements.queryAllWithConfig(destNavEntryNode).map((item) => [item.name, item]));
+      const sharedElementNames = set_intersection(new Set(fromSharedElementMap.keys()), destSharedElementMap);
+
+      const sharedElementPages = [
+        {pageItem: dest, eleMap: destSharedElementMap},
+        {pageItem: from, eleMap: fromSharedElementMap},
+      ];
+      match(lifecycle)
+        .with('first', () => sharedElementPages)
+        .with('last', () => sharedElementPages.reverse())
         .exhaustive();
 
-      for (const pageItem of sharedElementPages) {
-        if (pageItem) {
-          sharedElementLifecycle.set(pageItem.navEntryNode, lifecycle);
-          const navEntryNode = pageItem.navEntryNode;
-          const zIndexStart = navEntryNode.sharedIndex;
-          this.__setSharedElement(sharedElementCss, navEntryNode, sharedElements.get(navEntryNode)!, sharedElementMap, `z-index:${zIndexStart};`);
-          for (const sharedItem of sharedElements.queryAllWithConfig(pageItem.navEntryNode)) {
-            this.__setSharedElement(sharedElementCss, sharedItem.element, sharedItem, sharedElementMap, `z-index:${sharedElementIndex};`);
-          }
+      for (const {pageItem, eleMap} of sharedElementPages) {
+        sharedElementLifecycle.set(pageItem.navEntryNode, lifecycle);
+        const navEntryNode = pageItem.navEntryNode;
+        const zIndexStart = navEntryNode.sharedIndex;
+        this.__setSharedElement(sharedElementCss, navEntryNode, sharedElements.get(navEntryNode)!, sharedElementMap, `z-index:${zIndexStart};`);
+        for (const sharedName of sharedElementNames) {
+          const sharedItem = eleMap.get(sharedName)!;
+          this.__setSharedElement(sharedElementCss, sharedItem.element, sharedItem, sharedElementMap, `z-index:${sharedElementIndex};`);
         }
       }
     }
