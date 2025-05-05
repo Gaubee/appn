eac3fb074b3fbad1ae87401f9f9e77bc68683b0e
Gaubee <GaubeeBangeel@Gmail.com>
2025-05-05 09:26:24 +0000

    Deploying to gh-pages from @ Gaubee/appn@9585d93e0353dab940f5c96c2e2c7115a6663689 🚀

--- assets/index-yBSFtCYJ.js
+++
@@ -1,5 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";import"./starting-style-demo-BRY0SUH6.js";import"./index-DG30SIpw.js";import"./css-color-mix-CY0KkXf5.js";class s extends HTMLElement{constructor(){super(),this.attachShadow({mode:"open"});const t=String.raw;this.shadowRoot.innerHTML=t`
-      <slot></slot>
-      <css-starting-style mode="native" slotted="" selector="div.native.showing" cssText="background-color: red!important;"></css-starting-style>
-      <css-starting-style mode="shim" slotted="" selector="div.shim.showing" cssText="background-color: red!important;"></css-starting-style>
-    `}}customElements.define("custom-div",s);
---
+++ assets/math-CrMaX9h8-CEFiPbik.js
@@ -0,0 +1,1 @@
+import"./index-lqWRYX0p-AhKfckNa.js";const h=(t,a,m)=>Math.min(m,Math.max(a,t));export{h as m};
