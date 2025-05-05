ebee5c457b2ddf91fac441d3fccca84ee8eecd0f
Gaubee <GaubeeBangeel@Gmail.com>
2025-05-05 15:15:42 +0000

    Deploying to gh-pages from @ Gaubee/appn@0f6057ed91d1733cff7001303c91fc087846974c 🚀

--- assets/index-CKDYvmYX.js
+++
@@ -1,5 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";import"./starting-style-demo-BRY0SUH6.js";import"./index-CGXPJn--.js";import"./shared-element-common-B8KgsPdL-CtM_MB7N.js";import"./index-lqWRYX0p-AhKfckNa.js";import"./promise-with-resolvers.polyfill-DmWLZHIp-CJy6J3Rc.js";import"./css-color-mix-BBMErY_8.js";import"./safe-property-CGUTARmz-Cf7enWfs.js";import"./iterable-egH69lT2-B-sgJhLT.js";import"./event-property-DHOI4cZw-CIovpyCQ.js";import"./math-CrMaX9h8-CEFiPbik.js";class s extends HTMLElement{constructor(){super(),this.attachShadow({mode:"open"});const t=String.raw;this.shadowRoot.innerHTML=t`
-      <slot></slot>
-      <css-starting-style mode="native" slotted="" selector="div.native.showing" cssText="background-color: red!important;"></css-starting-style>
-      <css-starting-style mode="shim" slotted="" selector="div.shim.showing" cssText="background-color: red!important;"></css-starting-style>
-    `}}customElements.define("custom-div",s);
